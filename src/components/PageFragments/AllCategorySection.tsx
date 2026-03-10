e3038909602f7536680b63e567ea436856c55f26
Christopher <ChristopherOhwavworua@transalliance.ng>
2026-03-10 02:43:09 +0100

    cat fixes

--- src/components/PageFragments/AllCategorySection.tsx
+++ src/components/PageFragments/AllCategorySection.tsx
@@ -197,67 +197,68 @@ const AllCategorySection = () => {
         Select category
       </h5>
       <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 mx-auto max-w-[1350px] px-2 lg:px-0  mt-6 gap-10">
-        {Categories?.slice(0, 5).map((cat) => {
-          const productImage = categoryProductsMap[cat?.id];
-          return (
-            <Link
-              key={cat.id}
-              href={`/category/${convertToSlug(cat.name)}-${cat.id}`}
-              className="group block w-full max-w-[300px] md:max-w-[220px]"
-            >
-              <div className="bg-[#EDEDED] rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-all duration-300">
-                {/* IMAGE SECTION */}
-                <div className="relative p-4">
-                  {/* Wishlist Icon */}
-                  <button
-                    className="absolute top-3 right-3 bg-white/80 backdrop-blur-sm 
+        {categories
+          ?.filter((category: CategoryType) => category?.count > 0)
+          ?.slice(0, 5)
+          .map((cat:any) => {
+            const productImage = categoryProductsMap[cat?.id];
+            return (
+              <Link
+                key={cat.id}
+                href={`/category/${convertToSlug(cat.name)}-${cat.id}`}
+                className="group block w-full max-w-[300px] md:max-w-[220px]"
+              >
+                <div className="bg-[#EDEDED] rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-all duration-300">
+                  {/* IMAGE SECTION */}
+                  <div className="relative p-4">
+                    {/* Wishlist Icon */}
+                    <button
+                      className="absolute top-3 right-3 bg-white/80 backdrop-blur-sm 
                          rounded-full p-2 shadow-sm hover:scale-105 transition"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-4 w-4 text-gray-500"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
                     >
-                      <path
-                        d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4 text-gray-500"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 
                    2 5.42 4.42 3 7.5 3 
                    c1.74 0 3.41.81 4.5 2.09 
                    C13.09 3.81 14.76 3 16.5 3 
                    19.58 3 22 5.42 22 8.5 
                    c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                        />
+                      </svg>
+                    </button>
+
+                    {/* Product Image */}
+                    <div className="flex items-center justify-center h-[200px]">
+                      <img
+                        src={cat.image?.src ?? productImage}
+                        alt={cat.image?.name}
+                        className="max-h-full object-contain transition-transform duration-500 group-hover:scale-105"
                       />
-                    </svg>
-                  </button>
-
-                  {/* Product Image */}
-                  <div className="flex items-center justify-center h-[200px]">
-                    <img
-                      src={cat.image?.src ?? productImage}
-                      alt={cat.image?.name}
-                      className="max-h-full object-contain transition-transform duration-500 group-hover:scale-105"
-                    />
+                    </div>
                   </div>
-                </div>
 
-                {/* INFO SECTION */}
-                <div className="px-4 pb-4 space-y-1">
-      
-
-                  {/* CTA BUTTON */}
-                  <button
-                    className=" w-full bg-gradient-to-r from-[#6D28D9] to-[#5E1EE5]
+                  {/* INFO SECTION */}
+                  <div className="px-4 pb-4 space-y-1">
+                    {/* CTA BUTTON */}
+                    <button
+                      className=" w-full bg-gradient-to-r from-[#6D28D9] to-[#5E1EE5]
                    text-white text-sm font-medium py-2.5 rounded-md
                    flex items-center justify-center gap-2
                    hover:opacity-90 transition"
-                  >
-                    {cat?.name}
-                  </button>
+                    >
+                      {cat?.name}
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          );
-        })}
+              </Link>
+            );
+          })}
       </div>
 
       {/* </Carousel> */}
